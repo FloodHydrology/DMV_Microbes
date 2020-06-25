fafe86f23cb467fcc3be210405d0ace8c06697b5
cnjones7 <>
2020-06-25 16:07:07 -0500

    updated plots

--- analysis/III_Plotting.R
+++ analysis/III_Plotting.R
@@ -17,40 +17,97 @@ library(lubridate)
 library(tidyverse)
 
 #load data
-df<-read_csv("data//waterLevel_cleaned.csv")
 depth<-read_csv("data//DepthToWaterTable.csv")
 metrics<-read_csv("data//annual_metrics.csv")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 2.0 Hydrologic Regime Plots-------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Wetland hydrograph~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-hyd<-df %>% 
+#2.1 Hydrograph---------------------------------------------
+df<-depth %>% 
   #seleect wetland well data
-  filter(str_detect(site, 'Wetland Well')) %>% 
-  #Group by date
-  group_by(day) %>% 
+  filter(str_detect(station, 'SC')) %>% 
+  #create sampling location col
+  mutate(loc = substr(station, 4,4)) %>% 
+  #Convert from depth to waterLevel
+  mutate(waterLevel = -1*d_n) %>% 
+  #Group by date & loc
+  group_by(day, loc) %>% 
   #Summarise water level data
   summarise(
     median = median(waterLevel, na.rm = T),
     lwr    = quantile(waterLevel, 0.25, na.rm = T), 
-    upr    = quantile(waterLevel, 0.75, na.rm = T)) %>% 
-  #Start ggplot
-  ggplot() + 
-    geom_hline(aes(yintercept=0), lty=2,lwd=1.25, col="grey30")+  
-    geom_ribbon(aes(ymin = lwr, ymax = upr, x = day), 
-                fill="#377eb8", 
-                col='grey30',
-                alpha=0.7) + 
-    geom_line(aes(y = median, x = day), col="grey30")+
-    theme_bw() + 
-      ylab("Wetland Water Level") + 
-      xlab(NULL) + 
-      scale_x_date(date_labels = "%b") +
-      theme(axis.title = element_text(size = 14),
-            axis.text = element_text(size = 12)) 
+    upr    = quantile(waterLevel, 0.75, na.rm = T))
 
-#Water Level Plot~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Subset by sampling location
+a<-df %>% filter(loc=='A')
+b<-df %>% filter(loc=='B')
+c<-df %>% filter(loc=='C')
+d<-df %>% filter(loc=='D')
+e<-df %>% filter(loc=="E")
+
+#Define ribbon tranparency
+ribbon_alpha<-0.90
+
+#Define colors
+cols<-c(
+  'A' = '#045a8d', 
+  'B' = '#2b8cbe', 
+  'C' = '#74a9cf', 
+  'D' = '#bdc9e1',
+  'E' = '#f1eef6')
+line_col<-"grey50"
+  
+#Start ggplot
+hyd<-ggplot() + 
+  geom_rect(
+    aes(
+      xmin = as.Date('2017-09-01'), 
+      xmax = as.Date('2018-10-31'), 
+      ymin = -0.30, 
+      ymax = 0), 
+    fill='grey70', alpha = 0.9) +
+  #E
+  geom_ribbon(aes(ymin = e$lwr, ymax = e$upr, x = e$day, fill='E'), 
+              col='grey90', lwd=0.25) +
+  geom_line(aes(x=e$day, y=e$median), 
+            col=line_col) +
+  #D
+  geom_ribbon(aes(ymin = d$lwr, ymax = d$upr, x = d$day, fill='D'),
+              alpha=ribbon_alpha) +
+  geom_line(aes(x=d$day, y=d$median), 
+            col=line_col) +
+  #C
+  geom_ribbon(aes(ymin = c$lwr, ymax = c$upr, x = c$day, fill='C'),
+              alpha=ribbon_alpha) +
+  geom_line(aes(x=c$day, y=c$median), 
+            col=line_col) +
+  #B
+  geom_ribbon(aes(ymin = b$lwr, ymax = b$upr, x = b$day, fill="B"), 
+              alpha=ribbon_alpha) +
+  geom_line(aes(x=b$day, y=b$median), 
+            col=line_col) +
+  #A
+  geom_ribbon(aes(ymin = a$lwr, ymax = a$upr, x = a$day, fill='A'),
+              alpha=ribbon_alpha) +
+  geom_line(aes(x=a$day, y=a$median), 
+            col=line_col) +
+  #Legend/color
+  scale_fill_manual(name=NULL, values=cols) +
+  #Clip to water year
+  coord_cartesian(xlim=as.Date(c("2017-10-01", "2018-09-30"))) +
+  #theme options
+  theme_bw() + 
+    ylab("Water Level [m]") + 
+    xlab(NULL) + 
+    scale_x_date(date_labels = "%b") +
+    theme(axis.title = element_text(size = 14),
+          axis.text = element_text(size = 12),
+          legend.position = c("bottom"), 
+          legend.margin = margin(t=-2, unit="lines"))
+
+
+#2.2 Water Level Plot---------------------------------------
 dep<-depth %>%
   #Convert depth-to-water-table to water level
   mutate(d_n = -1*d_n) %>% 
@@ -76,10 +133,11 @@ dep<-depth %>%
                alpha = 70) + 
     theme_bw() + 
       ylab("Median Water Level [m]") + 
-      xlab(NULL) + 
+      xlab("Sampling Location") + 
       theme(axis.title = element_text(size = 14),
             axis.text = element_text(size = 12)) 
 
+#2.3 Duration ----------------------------------------------
 #Duration plot
 dur<-metrics %>% 
   #Select SC stations
@@ -103,11 +161,11 @@ dur<-metrics %>%
              alpha = 70) + 
   theme_bw() +
   ylab("Saturation Duration [Days]") + 
-  xlab(NULL) + 
+  xlab('Sampling Location') + 
   theme(axis.title = element_text(size = 14),
         axis.text = element_text(size = 12)) 
 
-#Frequency plot
+#2.4 Frequency----------------------------------------------
 freq<-metrics %>% 
   #Select SC stations
   filter(str_detect(station, 'SC')) %>% 
@@ -130,11 +188,11 @@ freq<-metrics %>%
              alpha = 70) + 
   theme_bw() + 
   ylab("Saturation Frequency [Events]") + 
-  xlab(NULL) + 
+  xlab("Sampling Location") + 
   theme(axis.title = element_text(size = 14),
         axis.text = element_text(size = 12)) 
 
-#Print plot~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-png("docs/hydro_regime.png", res=300, width = 7, height = 5.5, units = 'in')
+#2.5 Print plot---------------------------------------------
+png("docs/hydro_regime.png", res=300, width = 7, height = 6, units = 'in')
 hyd + dep + dur + freq + plot_layout(ncol=2)
 dev.off()
